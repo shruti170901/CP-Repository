bc27f45dcf3cf9f39d3c3fbb06b5776d1e96db11
shruti170901 <58871601+shruti170901@users.noreply.github.com>
2020-05-14 16:15:56 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <pair>
+using namespace std;
+#define vi  vector<int >
+#define vii vector<vi >
+#define ll long long int
+#define all@ (@).begin(),(@).end()
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+    
+    
+    }
+    return 0;
+}
